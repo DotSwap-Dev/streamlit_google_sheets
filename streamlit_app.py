8d5da60581801a0da371ea424864dfe6c43b5c5a
DotSwap-Dev <163226521+DotSwap-Dev@users.noreply.github.com>
2024-06-07 16:40:08 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -13,12 +13,12 @@ dataframe = pd.DataFrame(google_sheets_table) # Convert google sheets table into
 Hi there! This is a simple Google Sheets example on how to embed a dynamic table on a Medium blog post with Streamlit! 
 """
 
-# Define tabs
-#tab1, tab2 = st.tabs(["Table", "Graph"])
+ Define tabs
+tab1, tab2 = st.tabs(["Table", "Graph"])
 
 # Streamlit content
-#with tab1:
-#  st.write(dataframe)
+with tab1:
+  st.write(dataframe)
 #with tab2:
 #  st.line_chart(dataframe,x="Date", y="Close")
 
